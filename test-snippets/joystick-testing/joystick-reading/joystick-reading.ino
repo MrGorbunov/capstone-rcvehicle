f41556e45907d0b367f17aa6cf75121ef5256738
MrGorbunov <volarcko@gmail.com>
2020-10-23 15:33:56 -0400

    Finished joystick testing code
    Created accompanying processing sketch to read serial & visualize joystick position.
    
    Got switch pinout of joystick to work by using analog read and checking for threshold.
    
    Renamed everything to fit within the same folder

--- test-snippets/joystick-testing/joystick-reading/joystick-reading.ino
+++ test-snippets/joystick-testing/joystick-reading/joystick-reading.ino
@@ -11,9 +11,13 @@
 
 const byte PIN_X = A0;
 const byte PIN_Y = A1;
-const byte PIN_SWITCH = 4;
+// Switch is a boolean, but its voltage is not correct
+// to trigger digital pin so instead an analog is used.
+// Some kind of Elec 1 solution would free up the analog pin.
+const byte PIN_SWITCH = A2;
+const int SWITCH_THRESHOLD = 10; // when pressed, analog goes to 0, so this allows for some tolerance
 
-const int samplePeriod = 1000;
+const int samplePeriod = 50;
 int switchDown;
 int x, y;
 
@@ -23,14 +27,15 @@ void setup() {
   Serial.begin(9600);
   Serial.println();
 
-  pinMode(PIN_SWITCH, INPUT);
   // Analog pins don't require pinMode()
 }
 
 void loop() {
   x = analogRead(PIN_X);
   y = analogRead(PIN_Y);
-  switchDown = digitalRead(PIN_SWITCH);
+  switchDown = 0;
+  if (analogRead(PIN_SWITCH) < SWITCH_THRESHOLD)
+    switchDown = 1;
 
   Serial.print(x);
   Serial.print(",");
