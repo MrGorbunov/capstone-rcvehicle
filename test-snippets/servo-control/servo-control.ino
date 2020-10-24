e8dad47d083b14fff6f3a5a6e8f39838e3e1a001
MrGorbunov <volarcko@gmail.com>
2020-10-24 13:27:53 -0400

    Did more servo testing

--- test-snippets/servo-control/servo-control.ino
+++ test-snippets/servo-control/servo-control.ino
@@ -8,8 +8,10 @@
   and the servos will spin to that angle. The servos
   with the library are limited to a 180 degree range.
 
-  Min angle is 0, max is 180. Putting values above that
-  doesn't break the motor but the behaviour is strange.
+  Min angle is 0, max is 180. All inputs are clamped
+  to that range. 
+   - 200, 360, 400 are treated as 180 deg
+   - negativea are treated as 0 deg
 
 */
 
@@ -28,19 +30,29 @@ const int STEP_ANGLE = 15;
 
 void setup() {
   pinMode(PIN_SERVO, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   servo.attach(PIN_SERVO);
 }
 
 void loop() {
-  for (pos=0; pos<180; pos++) {
+  for (pos=0; pos<90; pos++) {
     servo.write(pos);
     delay(STEP_DELAY);
   }
+
+  delay(500);
+  // The servo treats 200 as 180
+  servo.write(200);
+  delay(500);
+  // 370 = 180
+  servo.write(370);
   delay(500);
 
-  for (pos=180; pos>0; pos--) {
-    servo.write(pos);
-    delay(STEP_DELAY);
-  }
-}
+  digitalWrite(LED_BUILTIN, HIGH);
+  // -30 = 0 deg
+  servo.write(-30);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
 
+  delay(1000);
+}
